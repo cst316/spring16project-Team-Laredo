363d97b9b395c8751bc5edc916a4b5c591a50c54
Christopher Ruiter <cruiter1@asu.edu>
2016-02-03 21:43:17 -0700

    Window Minimizes and Restores correctly. Exit button still closes frame but doesn't exit VM

--- src/net/sf/memoranda/ui/AppFrame.java
+++ src/net/sf/memoranda/ui/AppFrame.java
@@ -665,8 +665,8 @@ public class AppFrame extends JFrame {
     }
 
     public void doMinimize() {
-        exitNotify();
-        App.closeWindow();
+        //exitNotify();
+        App.minimizeWindow();
     }
 
     //Help | About action performed
@@ -680,21 +680,21 @@ public class AppFrame extends JFrame {
          dlg.setVisible(true);
     }
 
-    protected void processWindowEvent(WindowEvent e) {
-        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
-            if (Configuration.get("ON_CLOSE").equals("exit"))
-                doExit();
-            else
-                doMinimize();
-        }
-        else if ((e.getID() == WindowEvent.WINDOW_ICONIFIED)) {
-            super.processWindowEvent(new WindowEvent(this,
-                    WindowEvent.WINDOW_CLOSING));
-            doMinimize();
-        }
-        else
-            super.processWindowEvent(e);
-    }
+//    protected void processWindowEvent(WindowEvent e) {
+//        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+//            if (Configuration.get("ON_CLOSE").equals("exit"))
+//                doExit();
+//            else
+//                doMinimize();
+//        }
+//        else if ((e.getID() == WindowEvent.WINDOW_ICONIFIED)) {
+//            super.processWindowEvent(new WindowEvent(this,
+//                    WindowEvent.WINDOW_CLOSING));
+//            doMinimize();
+//        }
+//        else
+//            super.processWindowEvent(e);
+//    }
 
     public static void addExitListener(ActionListener al) {
         exitListeners.add(al);
