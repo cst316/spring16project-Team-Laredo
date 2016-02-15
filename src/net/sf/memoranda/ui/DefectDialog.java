66920567099ace9a24fefabc7464e13482cc3b3d
tjcole2 <tjcole2@asu.edu>
2016-02-14 18:47:02 -0700

    Modified DefectDialog GUI

--- src/net/sf/memoranda/ui/DefectDialog.java
+++ src/net/sf/memoranda/ui/DefectDialog.java
@@ -26,6 +26,15 @@ import javax.swing.event.ChangeListener;
 
 import net.sf.memoranda.CurrentProject;
 import net.sf.memoranda.date.CalendarDate;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
 
 /**
  * JDialog box that allows users to enter in information
@@ -68,7 +77,7 @@ public class DefectDialog extends JDialog{
 	
 	JPanel descriptionPanel = new JPanel();
 	
-	JLabel descriptionLabel = new JLabel("Description");
+	JLabel descriptionLabel = new JLabel("Defect Description:");
 	JTextArea descriptionTextArea = new JTextArea();
 	
 	JPanel dateFoundInputPanel = new JPanel();
@@ -89,20 +98,19 @@ public class DefectDialog extends JDialog{
 	JPanel phasesPanel = new JPanel();
 	
 	JLabel injectionPhaseLabel = new JLabel("Injection Phase:");
-	public JComboBox<String> injectionPhaseComboBox = new JComboBox<String>(phases);
+	public JComboBox injectionPhaseComboBox = new JComboBox(phases);
 	
 	JLabel removalPhaseLabel = new JLabel("Removal Phase:");
-	JComboBox<String> removalPhaseComboBox = new JComboBox<String>(phases);
-	
-	JPanel typePanel = new JPanel();
+	JComboBox removalPhaseComboBox = new JComboBox(phases);
 	
 	JLabel typeLabel = new JLabel("Type of Defect:");
-	JComboBox<String> typeOfDefect = new JComboBox<String>(types);
+	JComboBox typeOfDefect = new JComboBox(types);
 	
 	JPanel buttonPanel = new JPanel();
 	
 	JButton okButton = new JButton("Ok");
 	JButton cancelButton = new JButton("Cancel");
+	private final JPanel southPanel = new JPanel();
 	
     public DefectDialog(Frame frame, String title){
     	super(frame, title, true);
@@ -116,41 +124,150 @@ public class DefectDialog extends JDialog{
     }
     
     public void init(){
-    	setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+    //GridBag Stuff
+    	GridBagConstraints gbc_descriptionLabel = new GridBagConstraints();
+			gbc_descriptionLabel.anchor = GridBagConstraints.WEST;
+			gbc_descriptionLabel.fill = GridBagConstraints.VERTICAL;
+			gbc_descriptionLabel.insets = new Insets(0, 0, 5, 0);
+			gbc_descriptionLabel.gridx = 0;
+			gbc_descriptionLabel.gridy = 0;
+		GridBagLayout gbl_descriptionPanel = new GridBagLayout();
+			gbl_descriptionPanel.columnWidths = new int[]{586, 0};
+			gbl_descriptionPanel.rowHeights = new int[]{32, 232, 0};
+			gbl_descriptionPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+			gbl_descriptionPanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		GridBagConstraints gbc_descriptionTextArea = new GridBagConstraints();
+	    	gbc_descriptionTextArea.fill = GridBagConstraints.BOTH;
+	    	gbc_descriptionTextArea.gridx = 0;
+	    	gbc_descriptionTextArea.gridy = 1;
+	    GridBagLayout gbl_phasesPanel = new GridBagLayout();
+    	  	gbl_phasesPanel.columnWidths = new int[]{86, 90, 82, 93, 86, 79, 0};
+    	  	gbl_phasesPanel.rowHeights = new int[]{69, 0};
+    	  	gbl_phasesPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+    	  	gbl_phasesPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+    	GridBagConstraints gbc_injectionPhaseLabel = new GridBagConstraints();
+    	  	gbc_injectionPhaseLabel.anchor = GridBagConstraints.EAST;
+    	  	gbc_injectionPhaseLabel.insets = new Insets(0, 0, 0, 5);
+    	  	gbc_injectionPhaseLabel.gridx = 0;
+    	  	gbc_injectionPhaseLabel.gridy = 0;
+    	GridBagConstraints gbc_injectionPhaseComboBox = new GridBagConstraints();
+    	  	gbc_injectionPhaseComboBox.fill = GridBagConstraints.HORIZONTAL;
+    	  	gbc_injectionPhaseComboBox.insets = new Insets(0, 0, 0, 5);
+    	  	gbc_injectionPhaseComboBox.gridx = 1;
+    	  	gbc_injectionPhaseComboBox.gridy = 0;
+    	GridBagConstraints gbc_removalPhaseLabel = new GridBagConstraints();
+    	  	gbc_removalPhaseLabel.anchor = GridBagConstraints.EAST;
+    	  	gbc_removalPhaseLabel.insets = new Insets(0, 0, 0, 5);
+    	  	gbc_removalPhaseLabel.gridx = 2;
+    	  	gbc_removalPhaseLabel.gridy = 0;
+    	GridBagConstraints gbc_removalPhaseComboBox = new GridBagConstraints();
+    	  	gbc_removalPhaseComboBox.insets = new Insets(0, 0, 0, 5);
+    	  	gbc_removalPhaseComboBox.fill = GridBagConstraints.HORIZONTAL;
+    	  	gbc_removalPhaseComboBox.gridx = 3;
+    	  	gbc_removalPhaseComboBox.gridy = 0;
+    	GridBagConstraints gbc_typeLabel = new GridBagConstraints();
+    	  	gbc_typeLabel.anchor = GridBagConstraints.EAST;
+    	  	gbc_typeLabel.insets = new Insets(0, 0, 0, 5);
+    	  	gbc_typeLabel.gridx = 4;
+    	  	gbc_typeLabel.gridy = 0;
+    	GridBagConstraints gbc_typeOfDefect = new GridBagConstraints();
+    	  	gbc_typeOfDefect.anchor = GridBagConstraints.WEST;
+    	  	gbc_typeOfDefect.gridx = 5;
+    	  	gbc_typeOfDefect.gridy = 0;
+    	GridBagLayout gbl_dateFoundInputPanel = new GridBagLayout();
+	    	gbl_dateFoundInputPanel.columnWidths = new int[]{86, 94, 49, 0};
+	    	gbl_dateFoundInputPanel.rowHeights = new int[]{25, 0};
+	    	gbl_dateFoundInputPanel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+	    	gbl_dateFoundInputPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+	    GridBagConstraints gbc_dateFoundLabel = new GridBagConstraints();
+	    	gbc_dateFoundLabel.insets = new Insets(0, 0, 0, 5);
+	    	gbc_dateFoundLabel.gridx = 0;
+	    	gbc_dateFoundLabel.gridy = 0;
+	    GridBagConstraints gbc_dateFoundSpinner = new GridBagConstraints();
+	    gbc_dateFoundSpinner.anchor = GridBagConstraints.EAST;
+	    	gbc_dateFoundSpinner.insets = new Insets(0, 0, 0, 5);
+	    	gbc_dateFoundSpinner.gridx = 1;
+	    	gbc_dateFoundSpinner.gridy = 0;
+	    GridBagConstraints gbc_dateFoundButton = new GridBagConstraints();
+	    	gbc_dateFoundButton.anchor = GridBagConstraints.NORTH;
+	    	gbc_dateFoundButton.gridx = 2;
+	    	gbc_dateFoundButton.gridy = 0;
+	    GridBagLayout gbl_dateRemovedInputPanel = new GridBagLayout();
+	    	gbl_dateRemovedInputPanel.columnWidths = new int[]{98, 86, 107, 33, 0};
+	    	gbl_dateRemovedInputPanel.rowHeights = new int[]{25, 0};
+	    	gbl_dateRemovedInputPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+	    	gbl_dateRemovedInputPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+	    GridBagConstraints gbc_dateRemovedCheckBox = new GridBagConstraints();
+	    	gbc_dateRemovedCheckBox.anchor = GridBagConstraints.EAST;
+	    	gbc_dateRemovedCheckBox.insets = new Insets(0, 0, 0, 5);
+	    	gbc_dateRemovedCheckBox.gridx = 0;
+	    	gbc_dateRemovedCheckBox.gridy = 0;
+	    GridBagConstraints gbc_dateRemovedLabel = new GridBagConstraints();
+	    	gbc_dateRemovedLabel.insets = new Insets(0, 0, 0, 5);
+	    	gbc_dateRemovedLabel.gridx = 1;
+	    	gbc_dateRemovedLabel.gridy = 0;
+	    GridBagConstraints gbc_dateRemovedSpinner = new GridBagConstraints();
+	    gbc_dateRemovedSpinner.anchor = GridBagConstraints.EAST;
+	    	gbc_dateRemovedSpinner.insets = new Insets(0, 0, 0, 5);
+	    	gbc_dateRemovedSpinner.gridx = 2;
+	    	gbc_dateRemovedSpinner.gridy = 0;
+	    GridBagConstraints gbc_dateRemovedButton = new GridBagConstraints();
+	    	gbc_dateRemovedButton.anchor = GridBagConstraints.NORTHWEST;
+	    	gbc_dateRemovedButton.gridx = 3;
+	    	gbc_dateRemovedButton.gridy = 0;
+	    	
+	descriptionLabel.setFont(new Font("Dialog", Font.BOLD, 14));
+    	
+    	setPreferredSize(new Dimension(600, 400));
     	setResizable(false);
+    	getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
     	getContentPane().add(mainPanel);
     	
-    	mainPanel.setLayout(new GridLayout(6, 1, 0, 0));
+    	mainPanel.setBorder(new EmptyBorder(3, 3, 3, 3));
+    	mainPanel.setLayout(new BorderLayout(0, 0));
+    	mainPanel.add(descriptionPanel, BorderLayout.NORTH);
+    	mainPanel.add(phasesPanel, BorderLayout.CENTER);
+    	mainPanel.add(southPanel, BorderLayout.SOUTH);
+    	
+    	descriptionPanel.setLayout(gbl_descriptionPanel);
+    	descriptionPanel.add(descriptionLabel, gbc_descriptionLabel);
+    	descriptionPanel.add(descriptionTextArea, gbc_descriptionTextArea);
     	
     	descriptionTextArea.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
     	descriptionTextArea.setSize(new Dimension(TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT));
     	
-    	descriptionPanel.add(descriptionLabel);
-    	descriptionPanel.add(descriptionTextArea);
-    	
-    	descriptionPanel.setLayout(new GridLayout(2, 1, 5, 5));
-    	mainPanel.add(descriptionPanel);
-    
-    	phasesPanel.add(injectionPhaseLabel);
-    	phasesPanel.add(injectionPhaseComboBox);
-    	phasesPanel.add(removalPhaseLabel);
-    	phasesPanel.add(removalPhaseComboBox);
-    	
-    	mainPanel.add(phasesPanel);
-    	
+    	phasesPanel.setBorder(null);
+	  	phasesPanel.setLayout(gbl_phasesPanel);	  	
+	  	phasesPanel.add(injectionPhaseLabel, gbc_injectionPhaseLabel);	  	
+	  	phasesPanel.add(injectionPhaseComboBox, gbc_injectionPhaseComboBox);	  	
+	  	phasesPanel.add(removalPhaseLabel, gbc_removalPhaseLabel);	  	
+	  	phasesPanel.add(removalPhaseComboBox, gbc_removalPhaseComboBox);	  	
+	  	phasesPanel.add(typeLabel, gbc_typeLabel);	  	
+	  	phasesPanel.add(typeOfDefect, gbc_typeOfDefect);
+    	  	
     	typeOfDefect.setSize(new Dimension(COMBO_BOX_WIDTH, COMBO_BOX_HEIGHT));
-  
+    	    	
+    	southPanel.setLayout(new BorderLayout(0, 0));
+    	southPanel.add(buttonPanel, BorderLayout.SOUTH);
+    	southPanel.add(dateFoundInputPanel, BorderLayout.WEST);
+    	southPanel.add(dateRemovedInputPanel, BorderLayout.CENTER);
     	
-    	typePanel.add(typeLabel);
-    	typePanel.add(typeOfDefect);
-    	mainPanel.add(typePanel);
-    	
-    	dateFoundSpinner.setSize(new Dimension(SPINNER_WIDTH, SPINNER_HEIGHT));
-    	dateFoundSpinner.setModel(new SpinnerDateModel(new Date(), null, null, Calendar.DAY_OF_WEEK));
+    	buttonPanel.add(okButton);
+    	buttonPanel.add(cancelButton);
+    	    	
+    	dateFoundInputPanel.setBorder(null);    	    	    	    	
+    	dateFoundInputPanel.setLayout(gbl_dateFoundInputPanel);
+    	dateFoundInputPanel.add(dateFoundLabel, gbc_dateFoundLabel);    	    	
+    	dateFoundSpinner.setPreferredSize(new Dimension(100, 25));
+    	dateFoundInputPanel.add(dateFoundSpinner, gbc_dateFoundSpinner);
+    	    	
+    	dateFoundSpinner.setSize(new Dimension(100, 25));
+    	dateFoundSpinner.setModel(new SpinnerDateModel(new Date(1455498893453L), null, null, Calendar.DAY_OF_WEEK));
     	dateFoundSpinner.setEditor(new JSpinner.DateEditor(dateFoundSpinner, sdf.toPattern()));
     	dateFoundSpinner.addChangeListener(new ChangeListener(){
 			public void stateChanged(ChangeEvent arg0) {
-				SpinnerDateModel sdm = new SpinnerDateModel((Date)dateFoundSpinner.getModel().getValue(),null,null,Calendar.DAY_OF_WEEK);
+				SpinnerDateModel sdm = new SpinnerDateModel((Date)dateFoundSpinner.getModel().getValue(),
+						null,null,Calendar.DAY_OF_WEEK);
 				dateFoundSpinner.setModel(sdm);
 
                 if (ignoreDateFoundChange)
@@ -174,38 +291,38 @@ public class DefectDialog extends JDialog{
                 ignoreDateFoundChange = false;	
 			}
     	});
-    	
-    	
+    	dateRemovedSpinner.setEnabled(false);
+    	dateRemovedSpinner.setPreferredSize(new Dimension(100, 25));
+    	    	
+    	dateRemovedSpinner.setEditor(new JSpinner.DateEditor(dateFoundSpinner, sdf.toPattern()));
+    	    	
+    	dateFoundInputPanel.add(dateFoundButton, gbc_dateFoundButton);    	    	
     	dateFoundButton.setIcon(new ImageIcon(TaskDialog.class.getResource("/net/sf/memoranda/ui/resources/icons/calendar.png")));
     	dateFoundButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				openDateFoundCal(e);
-	        }
-	    });
-    	
-    	dateFoundCalFrame.cal.addSelectionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				if(ignoreDateFoundChange){
-					return;
-				}
-				dateFoundSpinner.getModel().setValue(dateFoundCalFrame.cal.get().getCalendar().getTime());
-			}
-    		
+    		public void actionPerformed(ActionEvent e) {
+    			openDateFoundCal(e);
+    		}
     	});
-    	
-    	dateRemovedInputPanel.add(dateFoundLabel);
-    	dateRemovedInputPanel.add(dateFoundSpinner);
-    	dateRemovedInputPanel.add(dateFoundButton);
-    	mainPanel.add(dateRemovedInputPanel);
-    	
-    	dateRemovedCheckBox.setSelected(false);
+    	    	
+    	    	
+    	dateRemovedInputPanel.setLayout(gbl_dateRemovedInputPanel);
+    	dateRemovedCheckBox.setHorizontalAlignment(SwingConstants.LEFT);    	    	
+    	dateRemovedInputPanel.add(dateRemovedCheckBox, gbc_dateRemovedCheckBox);    	    	
+    	dateRemovedCheckBox.setSelected(false);    	    	
+    	dateRemovedCheckBox.addActionListener(new ActionListener(){
+    		public void actionPerformed(ActionEvent e){
+    			checkBox_actionPerformed(e);
+    		}
+    	});
+    	    	
+    	dateRemovedLabel.setHorizontalAlignment(SwingConstants.CENTER);
+    	dateRemovedInputPanel.add(dateRemovedLabel, gbc_dateRemovedLabel);
     	
     	dateRemovedLabel.setEnabled(false);
     	
-    	dateRemovedSpinner.setEnabled(false);
+    	dateRemovedInputPanel.add(dateRemovedSpinner, gbc_dateRemovedSpinner);
     	dateRemovedSpinner.setSize(new Dimension(SPINNER_WIDTH, SPINNER_HEIGHT));
-    	dateRemovedSpinner.setModel(new SpinnerDateModel(new Date(), null, null, Calendar.DAY_OF_WEEK));
-    	dateRemovedSpinner.setEditor(new JSpinner.DateEditor(dateFoundSpinner, sdf.toPattern()));
+    	dateRemovedSpinner.setModel(new SpinnerDateModel(new Date(1455498633534L), null, null, Calendar.DAY_OF_WEEK));
     	dateRemovedSpinner.addChangeListener(new ChangeListener(){
 			public void stateChanged(ChangeEvent arg0) {
 				SpinnerDateModel sdm = new SpinnerDateModel((Date)dateRemovedSpinner.getModel().getValue(),null,null,Calendar.DAY_OF_WEEK);
@@ -216,7 +333,7 @@ public class DefectDialog extends JDialog{
                 ignoreDateRemovedChange = true;
                 Date sd = (Date) dateFoundSpinner.getModel().getValue();
                 Date ed = (Date) dateRemovedSpinner.getModel().getValue();				
-				if (ed.before(sd)) {
+				/*if (ed.before(sd)) {
 					dateRemovedSpinner.getModel().setValue(ed);
                     ed = sd;
                 }
@@ -230,9 +347,11 @@ public class DefectDialog extends JDialog{
                 }
                 dateRemovedCalFrame.cal.set(new CalendarDate(ed));
                 ignoreDateRemovedChange = false;
-				
+				*/
 			}
     	});
+    	    	
+    	dateRemovedInputPanel.add(dateRemovedButton, gbc_dateRemovedButton);
     	
     	dateRemovedButton.setEnabled(false);
     	dateRemovedButton.setIcon(new ImageIcon(TaskDialog.class.getResource("/net/sf/memoranda/ui/resources/icons/calendar.png")));
@@ -242,6 +361,16 @@ public class DefectDialog extends JDialog{
 	        }
 	    });
     	
+    	dateFoundCalFrame.cal.addSelectionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				if(ignoreDateFoundChange){
+					return;
+				}
+				dateFoundSpinner.getModel().setValue(dateFoundCalFrame.cal.get().getCalendar().getTime());
+			}
+    		
+    	});
+    	
     	dateRemovedCalFrame.cal.addSelectionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
     			if(ignoreDateRemovedChange){
@@ -250,18 +379,6 @@ public class DefectDialog extends JDialog{
     			dateRemovedSpinner.getModel().setValue(dateRemovedCalFrame.cal.get().getCalendar().getTime());
     		}
     	});
-    	
-    	dateRemovedCheckBox.addActionListener(new ActionListener(){
-    		public void actionPerformed(ActionEvent e){
-    			checkBox_actionPerformed(e);
-    		}
-    	});
-    	
-    	dateFoundInputPanel.add(dateRemovedCheckBox);
-    	dateFoundInputPanel.add(dateRemovedLabel);
-    	dateFoundInputPanel.add(dateRemovedSpinner);
-    	dateFoundInputPanel.add(dateRemovedButton);
-    	mainPanel.add(dateFoundInputPanel);
     	
     	okButton.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
@@ -274,10 +391,6 @@ public class DefectDialog extends JDialog{
     			ignoreAddingDefect(e);
     		} 	
     	});
-
-    	buttonPanel.add(okButton);
-    	buttonPanel.add(cancelButton);
-    	mainPanel.add(buttonPanel);
     }
     
 	void checkBox_actionPerformed(ActionEvent e) {
